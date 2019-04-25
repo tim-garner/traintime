ab9989bfc3efc13c8552e063fb52ce4fbbe1ae89
Timothy Garner <timothy.garner602@gmail.com>
2019-04-25 13:56:04 -0400

    This is an update

--- assets/javascript/tt.js
+++ assets/javascript/tt.js
@@ -67,4 +67,27 @@ database.ref().on("child_added", function(snapshot){
 
 }
 
+var todaysDate = "04/25/2019";
+console.log(todaysDate);
 
+function startTime()
+{
+var today=new Date();
+var h=today.getHours();
+var m=today.getMinutes();
+var s=today.getSeconds();
+// add a zero in front of numbers<10
+m=checkTime(m);
+s=checkTime(s);
+document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+t=setTimeout('startTime()',500);
+}
+function checkTime(i)
+{
+if (i<10)
+{
+i="0" + i;
+}
+return i;
+}
+console.log(startTime);
